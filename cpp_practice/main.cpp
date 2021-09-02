6e49a5711164989e31e70aeb7dd88f7159ac57f0
shaunchuang <72535297+shaunchuang@users.noreply.github.com>
2021-09-02 21:41:54 +0800

    Initial Commit

---
+++ cpp_practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_practice
+//
+//  Created by Shaun Chuang on 2021/9/2.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
